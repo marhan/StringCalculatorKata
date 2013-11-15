0559be65e9465dc9743a4a3766301a33a4cbd77d
Markus Hanses <me@markushanses>
2013-11-15 21:51:01 +0100

    first test for empty input created.

--- src/test/java/de/kata/StringCalculatorTest.java
+++ src/test/java/de/kata/StringCalculatorTest.java
@@ -3,6 +3,9 @@ package de.kata;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * @author markus.hanses
  */
@@ -16,7 +19,12 @@ public class StringCalculatorTest {
     }
 
     @Test
-    public void add() {
-        //TODO begin here
+    public void emptyStringResultsToZero() {
+        assertThat(subject.add(""), is(0));
+    }
+
+    @Test
+    public void nullResultsToZero() {
+        assertThat(subject.add(null), is(0));
     }
 }
