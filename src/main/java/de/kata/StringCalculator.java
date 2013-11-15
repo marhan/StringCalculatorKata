24f5b7c75622d9252647b8d673b22c54237e736f
Markus Hanses <me@markushanses>
2013-11-15 23:06:39 +0100

    Tests for delimiters added.

--- src/main/java/de/kata/StringCalculator.java
+++ src/main/java/de/kata/StringCalculator.java
@@ -5,9 +5,21 @@ package de.kata;
  */
 public class StringCalculator {
 
-    public int add(String numbers){
-        return 0;
-    }
+	public int add(String numbers) {
+		if (numbers == null || numbers.isEmpty()) {
+			return 0;
+		} else if (numbers.contains(",")) {
+			int sum = 0;
+			for (String number : numbers.split(",")) {
+				sum += toInt(number);
+			}
+			return sum;
+		} else {
+			return toInt(numbers);
+		}
+	}
 
-
+	private int toInt(String number) {
+		return Integer.parseInt(number);
+	}
 }
--- src/test/java/de/kata/StringCalculatorTest.java
+++ src/test/java/de/kata/StringCalculatorTest.java
@@ -11,20 +11,30 @@ import static org.junit.Assert.assertThat;
  */
 public class StringCalculatorTest {
 
-    private StringCalculator subject;
+	private StringCalculator subject;
 
-    @Before
-    public void setup() {
-        subject = new StringCalculator();
-    }
+	@Before
+	public void setup() {
+		subject = new StringCalculator();
+	}
 
-    @Test
-    public void emptyStringResultsToZero() {
-        assertThat(subject.add(""), is(0));
-    }
+	@Test
+	public void emptyStringResultsToZero() {
+		assertThat(subject.add(""), is(0));
+	}
 
-    @Test
-    public void nullResultsToZero() {
-        assertThat(subject.add(null), is(0));
-    }
+	@Test
+	public void nullResultsToZero() {
+		assertThat(subject.add(null), is(0));
+	}
+
+	@Test
+	public void oneGivenNumberIsReturned() {
+		assertThat(subject.add("1"), is(1));
+	}
+
+	@Test
+	public void twoCommaSeparatedNumbersWillReturnedAsSum() {
+		assertThat(subject.add("1,2"), is(3));
+	}
 }
