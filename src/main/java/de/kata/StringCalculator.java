766bf7040b92ce868f33fd9a806203d9f7a61598
Markus Hanses <me@markushanses>
2013-11-16 20:20:03 +0100

    Remove already implemented code.

--- src/main/java/de/kata/StringCalculator.java
+++ src/main/java/de/kata/StringCalculator.java
@@ -1,25 +1,5 @@
 package de.kata;
 
-/**
- * @author markus.hanses
- */
 public class StringCalculator {
 
-	public int add(String numbers) {
-		if (numbers == null || numbers.isEmpty()) {
-			return 0;
-		} else if (numbers.contains(",")) {
-			int sum = 0;
-			for (String number : numbers.split(",")) {
-				sum += toInt(number);
-			}
-			return sum;
-		} else {
-			return toInt(numbers);
-		}
-	}
-
-	private int toInt(String number) {
-		return Integer.parseInt(number);
-	}
 }
--- src/test/java/de/kata/StringCalculatorTest.java
+++ src/test/java/de/kata/StringCalculatorTest.java
@@ -1,14 +1,7 @@
 package de.kata;
 
 import org.junit.Before;
-import org.junit.Test;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
-
-/**
- * @author markus.hanses
- */
 public class StringCalculatorTest {
 
 	private StringCalculator subject;
@@ -18,23 +11,4 @@ public class StringCalculatorTest {
 		subject = new StringCalculator();
 	}
 
-	@Test
-	public void emptyStringResultsToZero() {
-		assertThat(subject.add(""), is(0));
-	}
-
-	@Test
-	public void nullResultsToZero() {
-		assertThat(subject.add(null), is(0));
-	}
-
-	@Test
-	public void oneGivenNumberIsReturned() {
-		assertThat(subject.add("1"), is(1));
-	}
-
-	@Test
-	public void twoCommaSeparatedNumbersWillReturnedAsSum() {
-		assertThat(subject.add("1,2"), is(3));
-	}
 }
