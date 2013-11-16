d2c754af74429a0501246a71dab432b42f799e89
Markus Hanses <me@markushanses>
2013-11-16 22:01:32 +0100

    Finished kata to step 5.

--- src/main/java/de/kata/StringCalculator.java
+++ src/main/java/de/kata/StringCalculator.java
@@ -1,22 +1,82 @@
 package de.kata;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * @author markus.hanses
  */
 public class StringCalculator {
 
 	public int add(String numbers) {
+		ArrayList<String> delimiters = new ArrayList<String>();
+		delimiters.add("\n");
+		delimiters.add(",");
+
+		List<String> negatives = new ArrayList<String>();
+		int sum = 0;
+
 		if (numbers == null || numbers.isEmpty()) {
-			return 0;
-		} else if (numbers.contains(",")) {
-			int sum = 0;
-			for (String number : numbers.split(",")) {
-				sum += toInt(number);
+			// do nothing
+		} else if (numbers.startsWith("//")) {
+			String withoutControlChar = numbers.replace("//", "");
+			String delimiter = String.valueOf(withoutControlChar.charAt(0));
+			String withoutNewLine = withoutControlChar.replace(delimiter + "\n", "");
+			delimiters.add(delimiter);
+			for (String number : split(delimiters, withoutNewLine)) {
+				int converted = toInt(number);
+				if (converted < 0) {
+					negatives.add(String.valueOf(converted));
+				}
+				sum += converted;
 			}
-			return sum;
 		} else {
-			return toInt(numbers);
+			for (String number : split(delimiters, numbers)) {
+				int converted = toInt(number);
+				if (converted < 0) {
+					negatives.add(String.valueOf(converted));
+				}
+				sum += converted;
+			}
 		}
+
+		if (!negatives.isEmpty()) {
+			throw new IllegalArgumentException("negatives not allowed: " + join(negatives));
+
+		}
+		return sum;
+	}
+
+	private String join(List<String> negatives) {
+		StringBuilder builder = new StringBuilder();
+		Iterator<String> iterator = negatives.iterator();
+		while (iterator.hasNext()) {
+			builder.append(iterator.next());
+			if (iterator.hasNext()) {
+				builder.append(",");
+			}
+		}
+		return builder.toString();
+	}
+
+	private List<String> split(List<String> delimiters, String... numbers) {
+		List<String> numberList = new ArrayList<String>(0);
+		for (String number : numbers) {
+			for (String splittedNumbers : splitWithDelimiter(delimiters.get(0), number)) {
+				numberList.add(splittedNumbers);
+			}
+		}
+		delimiters.remove(0);
+		if (!delimiters.isEmpty()) {
+			numberList = split(delimiters, numberList.toArray(new String[0]));
+		}
+		return numberList;
+	}
+
+	private List<String> splitWithDelimiter(String delimiterTwo, String firstSplit) {
+		return Arrays.asList(firstSplit.split(delimiterTwo));
 	}
 
 	private int toInt(String number) {
--- src/test/java/de/kata/StringCalculatorTest.java
+++ src/test/java/de/kata/StringCalculatorTest.java
@@ -3,8 +3,10 @@ package de.kata;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 /**
  * @author markus.hanses
@@ -36,5 +38,45 @@ public class StringCalculatorTest {
 	@Test
 	public void twoCommaSeparatedNumbersWillReturnedAsSum() {
 		assertThat(subject.add("1,2"), is(3));
+	}
+
+	@Test
+	public void anyCommaSeparatedNumbersWillReturnedAsSum() {
+		assertThat(subject.add("1,2,3,4,5,6,7,8"), is(36));
+	}
+
+	@Test
+	public void twoNewLineSeparatedNumberWillReturnedAsSum() {
+		assertThat(subject.add("1\n2"), is(3));
+	}
+
+	@Test
+	public void anyCommaAndNewLineSeparatedNumberWillReturnedAsSum() {
+		assertThat(subject.add("1,2\n3"), is(6));
+	}
+
+	@Test
+	public void anyDelimiterSeparatedNumberWillReturnedAsSum() {
+		assertThat(subject.add("//;\n1;2"), is(3));
+	}
+
+	@Test
+	public void negativeNumberWillOccurException() {
+		try {
+			subject.add("-1");
+			fail("exception expected");
+		} catch (Exception e) {
+			assertThat(e.getMessage(), containsString("negatives not allowed"));
+		}
+	}
+
+	@Test
+	public void negativeNumbersWillOccurExceptionAndContainedInMessage() {
+		try {
+			subject.add("-1,1,-2");
+			fail("exception expected");
+		} catch (Exception e) {
+			assertThat(e.getMessage(), is("negatives not allowed: -1,-2"));
+		}
 	}
 }
