605fea14628596c07cc1a0baa6c94c79cf33baf0
Markus Hanses <me@markushanses>
2013-11-20 23:19:03 +0100

    Completed

--- src/main/java/de/kata/StringCalculator.java
+++ src/main/java/de/kata/StringCalculator.java
@@ -1,5 +1,76 @@
 package de.kata;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class StringCalculator {
 
+    public int add(String input) {
+        String separatorRegex = ",|\n";
+
+        if (input.isEmpty()) {
+            return 0;
+        }
+        if (input.startsWith("//")) {
+            String separator = extractSeparator(input);
+            separatorRegex = addNewSeparator(separatorRegex, separator);
+            input = removeControl(input);
+        }
+
+        String[] numbers = input.split(separatorRegex);
+
+        return computeSum(numbers);
+    }
+
+    private int computeSum(String[] numbers) {
+        int sum = 0;
+        List<Integer> negatives = new ArrayList<Integer>();
+        for (String number : numbers) {
+            int convertedNumber = Integer.parseInt(number);
+            if (convertedNumber < 0) {
+                negatives.add(convertedNumber);
+            }
+
+            sum += convertedNumber;
+        }
+
+        if (!negatives.isEmpty()) {
+            return buildExceptionMessageAndThrow(negatives);
+        }
+
+        return sum;
+    }
+
+    private String addNewSeparator(String separatorRegex, String separator) {
+        separatorRegex = separatorRegex.concat("|").concat(separator);
+        return separatorRegex;
+    }
+
+    private String extractSeparator(String input) {
+        int endOfControl = findEndOfControl(input);
+        return input.substring(0, endOfControl).replace("//", "").replace("\n", "");
+    }
+
+    private int findEndOfControl(String input) {
+        return input.indexOf("\n") + 1;
+    }
+
+    private String removeControl(String input) {
+        int endOfControl = findEndOfControl(input);
+        return input.substring(endOfControl, input.length());
+    }
+
+    private int buildExceptionMessageAndThrow(List<Integer> negatives) {
+        StringBuilder builder = new StringBuilder();
+
+        Iterator<Integer> iterator = negatives.iterator();
+        while (iterator.hasNext()) {
+            builder.append(iterator.next());
+            if (iterator.hasNext()) {
+                builder.append(",");
+            }
+        }
+        throw new RuntimeException("negatives not allowed: " + builder.toString());
+    }
 }
--- src/test/java/de/kata/StringCalculatorTest.java
+++ src/test/java/de/kata/StringCalculatorTest.java
@@ -1,14 +1,81 @@
 package de.kata;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 public class StringCalculatorTest {
 
-	private StringCalculator subject;
+    private StringCalculator subject;
 
-	@Before
-	public void setup() {
-		subject = new StringCalculator();
-	}
+    @Before
+    public void setup() {
+        subject = new StringCalculator();
+    }
+
+    @Test
+    public void givenIsAnEmptyStringThenZeroIsReturned() {
+        assertThat(subject.add(""), is(0));
+    }
+
+    @Test
+    public void givenIsANumberThenTheNumberWillBeReturned() {
+        assertThat(subject.add("1"), is(1));
+    }
+
+    @Test
+    public void givenAreCommaSeparatedNumbersThenTheSumIsReturned() {
+        assertThat(subject.add("1,2"), is(3));
+    }
+
+    @Test
+    public void givenAreManyCommaSeparatedNumbersThenSumIsReturned() {
+        assertThat(subject.add("10,12,33"), is(55));
+    }
+
+    @Test
+    public void givenAreNewLineSeparatedNumbersThenSumIsReturned() {
+        assertThat(subject.add("1\n2"), is(3));
+    }
+
+    @Test
+    public void givenAreNewLineAndCommaSeparatedNumbersThenSumIsReturned() {
+        assertThat(subject.add("1\n2,3,10"), is(16));
+    }
+
+    @Test
+    public void givenAreCommaSeparatedNumbersWithControlCharacterThenSumIsReturned() {
+        assertThat(subject.add("//,\n2,3,10"), is(15));
+    }
+
+    @Test
+    public void givenAreSemicolonSeparatedNumbersWithControlCharacterThenSumIsReturned() {
+        assertThat(subject.add("//;\n2;3;10"), is(15));
+    }
+
+    @Test
+    public void givenIsNegativeNumberThenExceptionIsReturned() {
+        try {
+            subject.add("-11");
+            fail("Exception expected");
+        } catch (Exception e) {
+            assertThat(e.getMessage(), containsString("negatives not allowed"));
+        }
+    }
+
+    @Test
+    public void givenAreNegativeNumbersThenExceptionIsReturnedWithNegatives() {
+        try {
+            subject.add("2,-11,-3,-4");
+            fail("Exception expected");
+        } catch (Exception e) {
+            assertThat(e.getMessage(), is("negatives not allowed: -11,-3,-4"));
+        }
+    }
+
 
 }
