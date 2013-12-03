e5cdca5aef3dc27aba764edb3532b0e8b7f005be
Markus Hanses <me@markushanses>
2013-12-03 21:41:23 +0100

    Step 1 and 2 finished

--- src/main/java/de/kata/StringCalculator.java
+++ src/main/java/de/kata/StringCalculator.java
@@ -2,4 +2,16 @@ package de.kata;
 
 public class StringCalculator {
 
+    public int add(String numbers) {
+        if (numbers == null || numbers.isEmpty()) {
+            return 0;
+        }
+        int sum = 0;
+        String[] splittedNumbers = numbers.split(",");
+        for (String splittedNumber : splittedNumbers) {
+            sum += Integer.parseInt(splittedNumber);
+        }
+
+        return sum;
+    }
 }
--- src/test/java/de/kata/StringCalculatorTest.java
+++ src/test/java/de/kata/StringCalculatorTest.java
@@ -1,14 +1,54 @@
 package de.kata;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class StringCalculatorTest {
 
-	private StringCalculator subject;
+    private StringCalculator subject;
 
-	@Before
-	public void setup() {
-		subject = new StringCalculator();
-	}
+    @Before
+    public void setup() {
+        subject = new StringCalculator();
+    }
+
+    @Test
+    public void whenNullGivenThanZeroReturned() {
+        int number = subject.add(null);
+        assertThat(number, is(0));
+    }
+
+    @Test
+    public void whenEmptyStringGivenThanZeroReturned() {
+        int number = subject.add("");
+        assertThat(number, is(0));
+    }
+
+    @Test
+    public void whenNumberOneGivenThanOneReturned() {
+        int number = subject.add("1");
+        assertThat(number, is(1));
+    }
+
+    @Test
+    public void whenNumberTwoGivenThanTwoReturned() {
+        int number = subject.add("2");
+        assertThat(number, is(2));
+    }
+
+    @Test
+    public void whenTwoNumbersCommaSeparatedGivenThanSumReturned() {
+        int sum = subject.add("1,2");
+        assertThat(sum, is(3));
+    }
+
+    @Test
+    public void whenManyNumberCommaSeparatedGivenThanSumReturned() {
+        int sum = subject.add("1,2,3,4,5,6,7,8,9");
+        assertThat(sum, is(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9));
+    }
 
 }
